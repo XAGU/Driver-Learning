49335ce8c77b1ac15ff9b820f52d863f9b33bcd0
XAGU <2422494482@qq.com>
2019-06-21 17:20:28 +0800

    add HideInterrupHook

--- HideInterrupHook/DriverEntry.c
+++ HideInterrupHook/DriverEntry.c
@@ -51,6 +51,9 @@ typedef struct _KGDTENTRY {   //gdt表的元素的结构里
 	} HighWord;
 } KGDTENTRY, *PKGDTENTRY;
 
+//global
+USHORT g_uFilterJmp[3];
+ULONG_PTR g_uOrigInterruptFunc;
 
 //关闭页只读保护
 void _declspec(naked) OffPageProtect()
@@ -80,17 +83,122 @@ void _declspec(naked) OnPageProtect()
 	}
 }
 
- 
+
+VOID _stdcall FilterInterrupt()
+{
+	KdPrint(("%s", (char*)PsGetCurrentProcess() + 0x16c));
+}
+
+__declspec(naked)
+VOID NewInterrupt3OfOrigBase()
+{
+	__asm
+	{
+		pushad
+		pushfd
+		push fs
+		push 0x30
+		pop fs
+		call FilterInterrupt
+		pop fs
+		popfd
+		popad
+
+		jmp g_uOrigInterruptFunc
+	}
+}
+
+__declspec(naked)
+VOID NewINterrupt3()
+{
+	__asm
+	{
+		jmp fword ptr[g_uFilterJmp]
+
+	}
+}
+
+ULONG_PTR GetInterrupt(ULONG_PTR InterruptIndex)
+{
+	IDTR idtr;
+	IDTENTRY	*pIdtEntry;
+	_asm SIDT	idtr;
+	pIdtEntry = (IDTENTRY*)MAKELONG(idtr.IDT_LOWbase, idtr.IDT_HIGbase);
+	return MAKELONG(pIdtEntry[InterruptIndex].LowOffset, pIdtEntry[InterruptIndex].HiOffset);
+}
+
+ULONG_PTR GetNewBase(ULONG_PTR NewInterruptFunc, ULONG_PTR OrigInterruptFunc)
+{
+	return (NewInterruptFunc - OrigInterruptFunc);
+}
+
+VOID SetInterrupt(ULONG_PTR InterrupIndex, ULONG_PTR uNewBase,BOOLEAN bisNew)
+{
+	ULONG_PTR			u_fnKeSetTimeIncrement;
+	UNICODE_STRING		usFuncName;
+	ULONG_PTR			u_index;
+	ULONG_PTR			*u_KiProcessorBlock;
+
+	IDTENTRY			*pIdtEntry;
+	PKGDTENTRY			pGdt;
+	RtlInitUnicodeString(&usFuncName, L"KeSetTimeIncrement");
+	u_fnKeSetTimeIncrement = (ULONG_PTR)MmGetSystemRoutineAddress(&usFuncName);
+	if (!MmIsAddressValid((PVOID)u_fnKeSetTimeIncrement))
+	{
+		return;
+	}
+	u_KiProcessorBlock = *(ULONG_PTR**)(u_fnKeSetTimeIncrement + 44);
+	u_index = 0;
+	while (u_KiProcessorBlock[u_index])
+	{
+		pIdtEntry = *(IDTENTRY**)(u_KiProcessorBlock[u_index] - 0xE8);
+		pGdt = *(PKGDTENTRY*)(u_KiProcessorBlock[u_index] - 0xE4);
+		OffPageProtect();
+		if (bisNew)
+		{
+			
+			pIdtEntry[InterrupIndex].selector = 0xA8;
+			RtlCopyMemory(&pGdt[21], &pGdt[1],sizeof(KGDTENTRY));
+			pGdt[21].BaseLow = (USHORT)(uNewBase & 0xffff);
+			pGdt[21].HighWord.Bytes.BaseMid = (UCHAR)((uNewBase >> 16) & 0xffff);
+			pGdt[21].HighWord.Bytes.BaseHi = (UCHAR)(uNewBase >> 24);
+		}
+		else
+		{
+			pIdtEntry[InterrupIndex].selector = 0x8;
+			memset(&pGdt[21], 0, sizeof(KGDTENTRY));
+		}
+		OnPageProtect();
+		u_index++;
+	}
+}
+
+VOID HookInterruptFunc(ULONG_PTR InterrupIndex,ULONG_PTR NewInterruptFunc)
+{
+	ULONG_PTR uNewBase;
+	g_uOrigInterruptFunc = GetInterrupt(InterrupIndex);
+	uNewBase = NewInterruptFunc - g_uOrigInterruptFunc;
+	*(ULONG_PTR*)g_uFilterJmp = (ULONG_PTR)NewInterrupt3OfOrigBase;
+	g_uFilterJmp[2] = 0x8;
+	SetInterrupt(InterrupIndex, uNewBase,TRUE);
+}
+
+VOID UnHookInterruptFunc(ULONG_PTR InterruptIndex)
+{
+	SetInterrupt(3, 0, FALSE);
+}
 
 NTSTATUS DriverUnLoad(PDRIVER_OBJECT pDriverObject)
 {
 	KdPrint(("驱动卸载成功！"));
+	UnHookInterruptFunc(3);
 	return STATUS_SUCCESS;
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 {
 	KdPrint(("驱动加载成功！"));
+	HookInterruptFunc(3, (ULONG_PTR)NewINterrupt3);
 	pDriverObject->DriverUnload = DriverUnLoad;
 	return STATUS_SUCCESS;
 }
