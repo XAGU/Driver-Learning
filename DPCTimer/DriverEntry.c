fb4feae3208c11e92baa68eaf216d84c63c2522c
XAGU <2422494482@qq.com>
2019-06-27 02:31:13 +0800

    updata EnumDpcTimer

--- DPCTimer/DriverEntry.c
+++ DPCTimer/DriverEntry.c
@@ -5,6 +5,12 @@
 KTIMER g_kTimer;
 KDPC g_kDpc;
 
+typedef struct _KTIMER_TABLE_ENTRY {
+	ULONG_PTR			Lock;
+	LIST_ENTRY			Entry;
+	LARGE_INTEGER		Time;
+}KTIMER_TABLE_ENTRY,*PKTIMER_TABLE_ENTRY;
+
 VOID DeferreRoutine(
 	_In_ struct _KDPC *Dpc,
 	_In_opt_ PVOID DeferredContext,
@@ -32,16 +38,63 @@ VOID CancelTimer()
 	KeCancelTimer(&g_kTimer);
 }
 
+VOID EnumDpcTimer()
+{
+	ULONG_PTR Index;
+	ULONG_PTR kPrcb;
+	PKTIMER pKtimer;
+	PLIST_ENTRY pList;
+	PKTIMER_TABLE_ENTRY kTimerTable;
+	__asm {
+		push eax
+		mov eax,fs:[0x20]
+		mov kPrcb,eax
+		pop eax
+	}
+	kTimerTable = (PKTIMER_TABLE_ENTRY)(kPrcb + 0x19A0);
+	for (Index = 0; Index < 512; Index++)
+	{
+		pList = kTimerTable[Index].Entry.Flink;
+		if (!MmIsAddressValid(pList))
+		{
+			continue;
+		}
+		__try {
+			while (pList != &kTimerTable[Index].Entry)
+			{
+				pKtimer = (PKTIMER)((ULONG_PTR)pList - 0x18);
+				if (MmIsAddressValid(pKtimer)&&
+					MmIsAddressValid(pKtimer->Dpc))
+				{
+					if (pKtimer->Period&0xF0000000)
+					{
+						break;
+					}
+					KdPrint(("%X----%d", pKtimer->Dpc->DeferredRoutine, pKtimer->Period));
+				}
+				else
+				{
+					break;
+				}
+				pList = pList->Flink;
+			}
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER)
+		{
+			continue;
+		}
+	}
+}
+
 NTSTATUS DriverUnLoad(PDRIVER_OBJECT pDriverObject)
 {
-	CancelTimer();
 	KdPrint(("驱动卸载成功！"));
 	return STATUS_SUCCESS;
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 {
-	SetDpcTimer();
+	EnumDpcTimer();
 	pDriverObject->DriverUnload = DriverUnLoad;
 	KdPrint(("驱动加载成功！"));
 	return STATUS_SUCCESS;
